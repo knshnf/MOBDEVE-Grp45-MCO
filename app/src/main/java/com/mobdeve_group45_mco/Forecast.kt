0e1bf69a575c753c9382d5519ab4ffe8a81dd25e
taps210 <114672065+taps210@users.noreply.github.com>
2024-10-19 15:14:53 +0800

    Added kotlin classes for data population, and renamed RV layout files

---
+++ app/src/main/java/com/mobdeve_group45_mco/Forecast.kt
@@ -0,0 +1,12 @@
+package com.mobdeve_group45_mco
+
+class Forecast(day: String, iconId: Int, tempRange: String) {
+    var day= day
+        private set
+
+    var iconId = iconId
+        private set
+
+    var tempRange = tempRange
+        private set
+}
---
+++ app/src/main/java/com/mobdeve_group45_mco/WeatherCondition.kt
@@ -0,0 +1,12 @@
+package com.mobdeve_group45_mco
+
+class WeatherCondition(hour: String, iconId: Int, temp: String) {
+    var hour= hour
+        private set
+
+    var iconId = iconId
+        private set
+
+    var temp = temp
+        private set
+}
