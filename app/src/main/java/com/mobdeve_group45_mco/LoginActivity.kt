40e64b459ffc53acce05f4a62bb96017f1c77934
Kenshin F <kenshin_fetalvero@dlsu.edu.ph>
2024-10-20 14:30:36 +0800

    refactor: place home, profile, search in fragments

--- app/src/main/java/com/mobdeve_group45_mco/LoginActivity.kt
+++ app/src/main/java/com/mobdeve_group45_mco/LoginActivity.kt
@@ -7,13 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.navigation.NavigationBarView
 
-class login_page : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +22,6 @@ class login_page : AppCompatActivity() {
         var signInBtn = findViewById<Button>(R.id.activity_login_btn_signIn)
         signInBtn.setOnClickListener {
             val navIntent = Intent(this, MainActivity::class.java)
-            navIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(navIntent)
         }
     }
--- app/src/main/java/com/mobdeve_group45_mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve_group45_mco/MainActivity.kt
@@ -1,53 +1,34 @@
 package com.mobdeve_group45_mco
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.navigation.NavigationBarView
-import com.mobdeve_group45_mco.databinding.ActivityMainBinding
-import com.mobdeve_group45_mco.hourlyWeather.HourlyAdapter
-import com.mobdeve_group45_mco.dailyWeather.DailyAdapter
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewBinding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        setContentView(R.layout.activity_main)
 
-        viewBinding.activityMainRvHours.adapter = HourlyAdapter(DataGenerator.hourlyWeatherData())
-        viewBinding.activityMainRvHours.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        viewBinding.activityMainRvDays.adapter = DailyAdapter(DataGenerator.dailyWeatherData())
-        viewBinding.activityMainRvDays.layoutManager = LinearLayoutManager(this)
+        val homeFragment=HomeFragment()
+        val profileFragment=ProfileFragment()
+        val searchFragment=SearchFragment()
 
-        var bottomNavigationView = findViewById<NavigationBarView>(R.id.bottomNavigationView)
+        setCurrentFragment(homeFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.miHome -> {
-                    val navIntent = Intent(this, MainActivity::class.java)
-                    navIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(navIntent)
-                    true
-                }
-                R.id.miSearch -> {
-                    val navIntent = Intent(this, Search::class.java)
-                    navIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(navIntent)
-                    true
-                }
-                R.id.miProfile -> {
-                    val navIntent = Intent(this, profile_page::class.java)
-                    navIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(navIntent)
-                    true
-                }
-
-                else -> false
+            when(it.itemId){
+                R.id.miHome->setCurrentFragment(homeFragment)
+                R.id.miSearch->setCurrentFragment(searchFragment)
+                R.id.miProfile->setCurrentFragment(profileFragment)
             }
+            true
         }
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
