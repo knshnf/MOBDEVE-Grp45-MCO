c328eb2ae05ab5241a774222d5ff5debef63ef25
GabaGaba <gabriel_muriel@dlsu.edu.ph>
2024-10-24 22:49:37 +0800

    feat: added designs to search and profile page, fixed placements of login email and password

--- app/src/main/java/com/mobdeve_group45_mco/DataGenerator.kt
+++ app/src/main/java/com/mobdeve_group45_mco/DataGenerator.kt
@@ -80,6 +80,76 @@ class DataGenerator {
                     454, R.drawable.eren, CustomDate(2023, 0, 10)
                 )
             )
+            data.add(
+                Post(
+                    "armin.armode.armedian", "Armin Arlert",
+                    "RT: Some people consider me a... #bomb",
+                    9999, R.drawable.armin, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "wonderboy", "Zeke Jaeger",
+                    "HUUUUUUH????",
+                    1, R.drawable.zeke, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "eldian.pride", "Falco Grice",
+                    "I'm so screwed...",
+                    13, R.drawable.falco, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "rudolph_the_reiner", "Reiner Braun",
+                    "@jaegermeister awit",
+                    0, R.drawable.reiner, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "jaegermeister", "Eren Jaeger",
+                    "@rudolph_the_reiner you're just like me, bro",
+                    454, R.drawable.eren, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "armin.armode.armedian", "Armin Arlert",
+                    "RT: Some people consider me a... #bomb",
+                    9999, R.drawable.armin, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "wonderboy", "Zeke Jaeger",
+                    "HUUUUUUH????",
+                    1, R.drawable.zeke, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "eldian.pride", "Falco Grice",
+                    "I'm so screwed...",
+                    13, R.drawable.falco, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "rudolph_the_reiner", "Reiner Braun",
+                    "@jaegermeister awit",
+                    0, R.drawable.reiner, CustomDate(2023, 0, 10)
+                )
+            )
+            data.add(
+                Post(
+                    "jaegermeister", "Eren Jaeger",
+                    "@rudolph_the_reiner you're just like me, bro",
+                    454, R.drawable.eren, CustomDate(2023, 0, 10)
+                )
+            )
             return data
         }
     }
